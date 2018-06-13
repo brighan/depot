28927fda083dda3fbc50149967415b8a73c39675
Brighan Santamaria <Brighan Santamaria>
2018-06-13 14:08:55 -0500

    Change :title field to allow a maximum of 80 chars

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  validates :title, length: {maximum: 80, message: 'cannot exceed 80 chars'}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|png|jpg)\Z}i,
     message: 'must be a URL for .gif, .png or .jpg image'
